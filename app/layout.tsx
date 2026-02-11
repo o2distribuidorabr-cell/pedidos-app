e2bd9c9cebed977134c9b3650c1d21ab6b44832a
Pedro Andrade <o2distribuidora.br@gmail.com>
2026-02-11 02:44:16 -0300

    fix: layout mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
